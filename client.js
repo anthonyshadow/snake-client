9c1bfcecd910dbcc88d4da2b883b54de6adc3966
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-25 18:11:04 +0000

    user input

--- client.js
+++ client.js
@@ -11,16 +11,28 @@ const connect = function() {
   // interpret incoming data as text
   conn.on(`data`, (data) => {
     console.log('', data);
-    
   });
   conn.setEncoding('utf8');  
 
 conn.on('connect', () => {
-  console.log('succefully connected');
+  console.log('successfully connected');
   conn.write('Name: ADS');
+
+  /*
+  setInterval(() => {
+    conn.write("Move: up");
+  },50)
+  */
+  conn.write("Move: up");
+  conn.write("Move: down");
+  conn.write("Move: left");
+  conn.write("Move: right");
 });
 
-  
+//conn.on('connect', () => {
+  //conn.write("Move: up");
+//});
+
 
   return conn;
 };
